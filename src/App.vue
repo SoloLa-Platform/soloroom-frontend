f965fe82d1a2c7f2440dda42feb6bde8e1029097
ykhorizon <ykhorizon.light@gmail.com>
2019-09-29 20:46:10 +0800

    initial commit - seperate frontend codes from backend repo.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Transcription/>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Transcription from '@/pages/transcription/Transcription.vue';
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Transcription,
+  },
+});
+</script>
+
+<style lang="scss">
+  
+</style>
