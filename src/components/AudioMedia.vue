d7bd6b3c861db501cd5bb89cde96f63fb61b35b3
ykhorizon <ykhorizon.light@gmail.com>
2019-09-30 17:17:05 +0800

    SR-105 - add .editorconfig, new dependency packages, example unit test

--- src/components/AudioMedia.vue
+++ src/components/AudioMedia.vue
@@ -17,10 +17,10 @@
   </div>
 </template>
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 export default Vue.extend({
-  
-})
+
+});
 </script>
 
 <style lang="scss" scoped>
--- src/pages/transcription/Transcription.test.ts
+++ src/pages/transcription/Transcription.test.ts
@@ -1,10 +1,20 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount, mount } from '@vue/test-utils';
 import Transcription from './Transcription.vue';
+
+const localVue = createLocalVue();
+jest.mock('opensheetmusicdisplay');
 
 describe('test Transcription page', () => {
     describe('render', () => {
-        it('should render page', () => {
-            const wrapper = shallowMount(Transcription);
+        it('should render components', () => {
+            const wrapper = shallowMount(Transcription, {
+              stubs: {
+                Editor: {
+                  name: 'Editor',
+                  template: '<div/>',
+                },
+              },
+            });
             expect(wrapper);
         });
     });
