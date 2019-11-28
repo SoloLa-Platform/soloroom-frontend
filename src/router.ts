bf430bd9e39b8a24369316e64e15a48b50977e1d
Jack Tseng <b99901190@gmail.com>
2019-11-28 17:09:48 +0800

    SR-131-user authentication

--- src/router.ts
+++ src/router.ts
@@ -3,14 +3,25 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'Transcription',
+      name: 'home',
+      component: () => import('./pages/Home.vue'),
+    },
+    {
+      path: '/transcription',
+      name: 'transcription',
       component: () => import('./pages/transcription/Transcription.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/authenticate',
+      name: 'authenticate',
+      component: () => import('./pages/Authenticate.vue'),
     },
     // {
     //   path: '/about',
@@ -22,3 +33,18 @@ export default new Router({
     // },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if user is not logged in and trying to access a restricted page
+  const publicPages = ['/authenticate', '/'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/authenticate');
+  }
+
+  next();
+});
+
+export default router;
