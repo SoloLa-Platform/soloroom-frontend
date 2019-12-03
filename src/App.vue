b06b5b1bb0292196c8855226e17fc36b0d116327
jacktseng0704 <45512754+jacktseng0704@users.noreply.github.com>
2019-12-03 21:51:38 +0800

    SR-131-user authentication (#6)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <NavigationBar/>
-    <!-- this will be router-view -->
-    <Transcription/>
+    <NavigationBar />
+    <router-view :class="{ page: !transcription }" />
   </div>
 </template>
+
 <script lang="ts">
 import Vue from 'vue';
-import Transcription from '@/pages/transcription/Transcription.vue';
 import NavigationBar from '@/components/NavigationBar.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
     NavigationBar,
-    Transcription,
-
+  },
+  computed: {
+    transcription() {
+      if (this.$route.path === '/transcription') {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
 });
 </script>
 
 <style lang="scss">
-body, html  {
-  margin: 0;
-  padding: 0;
-}
+@import './assets/styles/global.scss';
 </style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,18 +1,85 @@
 <template>
-  <div class="navigation-bar">   
+  <div class="navigation-bar">
+    <router-link to="/" class="text">Solola</router-link>
+
+    <template v-if="user">
+      <router-link to="transcription">Transcription</router-link>
+
+      <span class="nav-welcome">Hello, {{ user.name }}.</span>
+
+      <button type="button" class="logoutButton" @click="logout">
+        Log out
+      </button>
+    </template>
+
+    <template v-else>
+      <router-link to="authenticate" class="button">Login</router-link>
+    </template>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-export default Vue.extend({
-  name: 'NavigationBar',
-});
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
+    },
+  },
+};
 </script>
+
 <style lang="scss" scoped>
-.navigation-bar { 
-  background: #FFFFFF;
-  height: 70px;
-  border-bottom: 1px solid rgb(224, 224, 224);
+.navigation-bar {
+  display: flex;
+  align-items: center;
+  min-height: 70px;
+  padding: 0.2em 1em;
+  background: linear-gradient(to right, #6f7474, #c9cec9);
+  .text {
+    font-size: 30px;
+  }
+}
+.nav-welcome {
+  margin-left: auto;
+  margin-right: 1rem;
+  color: white;
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+  margin: auto 0.8em auto 0.4em;
+  text-decoration: none;
+  border-top: 2px solid transparent;
+  border-bottom: 2px solid transparent;
+}
+
+.router-link-exact-active {
+  color: white;
+}
+
+button,
+.button {
+  margin-left: auto;
+  background: white;
+  text-decoration: none;
+  color: #2c3e50;
+
+  &.router-link-active {
+    color: #2c3e50;
+  }
+}
+
+.logoutButton {
+  cursor: pointer;
+}
+
+.nav-welcome + button {
+  margin-left: 0;
 }
 </style>
