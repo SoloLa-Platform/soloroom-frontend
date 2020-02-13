6d4b78f23cf4bcce1a96350761f8da2dedae0a1b
jacktseng0704 <b99901190@gmail.com>
2020-02-13 17:37:11 +0800

    SR-131- Add bootstrap

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,8 +1,43 @@
 <template>
-  <div class="navigation-bar">
-    <router-link to="/" class="text">Solola</router-link>
+  <nav id="navbar" class="navbar navbar-expand-md navbar-dark">
+    <router-link to="/" class="text navbar-brand"
+      ><span class="logo">Solo</span>La</router-link
+    >
 
-    <template v-if="user">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <form class="form-inline my-2 my-lg-0">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search solo tab!"
+          aria-label="Search"
+        />
+        <button class="btn btn-secondary" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <router-link to="about" class="button" href="#">About</router-link>
+        </li>
+        <li class="nav-item active">
+          <router-link to="authenticate" class="button">Login</router-link>
+          <!-- <a onclick="login()" class="nav-link" href="#">Login</a> -->
+        </li>
+      </ul>
+    </div>
+
+    <!-- <template v-if="user">
       <router-link to="transcription">Transcription</router-link>
 
       <span class="nav-welcome">Hello, {{ user.name }}.</span>
@@ -14,8 +49,8 @@
 
     <template v-else>
       <router-link to="authenticate" class="button">Login</router-link>
-    </template>
-  </div>
+    </template> -->
+  </nav>
 </template>
 
 <script>
@@ -24,62 +59,88 @@ export default {
     user() {
       return this.$store.state.user;
     },
-  },
-  methods: {
-    logout() {
-      this.$store.dispatch('logout');
-    },
-  },
-};
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.navigation-bar {
-  display: flex;
-  align-items: center;
-  min-height: 70px;
-  padding: 0.2em 1em;
-  background: linear-gradient(to right, #6f7474, #c9cec9);
-  .text {
-    font-size: 30px;
-  }
+#navbar {
+  background-color: rgb(255, 126, 66);
+  padding: 0 3%;
 }
+
+.logo {
+  color: #58d658;
+}
+
+// .navigation-bar {
+//   display: flex;
+//   align-items: center;
+//   min-height: 70px;
+//   padding: 0.2em 1em;
+//   background: linear-gradient(to right, #6f7474, #c9cec9);
+//   .text {
+//     font-size: 30px;
+//   }
+// }
+
+
+/* Navigation Bar */
+
+.navbar-brand {
+  font-family: sans-serif;
+  font-size: 2.5rem;
+  font-weight: bold;
+}
+
+.nav-item {
+  padding: 0 18px;
+}
+
+.button {
+  font-size: 1.2rem;
+  font-family: sans-serif;
+  font-weight: bold;
+  color: #fff;
+  margin: auto;
+}
+
 .nav-welcome {
   margin-left: auto;
   margin-right: 1rem;
   color: white;
 }
 
-a {
-  font-weight: bold;
-  color: #2c3e50;
-  margin: auto 0.8em auto 0.4em;
-  text-decoration: none;
-  border-top: 2px solid transparent;
-  border-bottom: 2px solid transparent;
-}
+// a {
+//   font-weight: bold;
+//   color: #2c3e50;
+//   margin: auto 0.8em auto 0.4em;
+//   text-decoration: none;
+//   border-top: 2px solid transparent;
+//   border-bottom: 2px solid transparent;
+// }
 
-.router-link-exact-active {
-  color: white;
-}
+// .router-link-exact-active {
+//   color: white;
+// }
 
-button,
-.button {
-  margin-left: auto;
-  background: white;
-  text-decoration: none;
-  color: #2c3e50;
+// button,
+// .button {
+//   margin-left: auto;
+//   background: white;
+//   text-decoration: none;
+//   color: #2c3e50;
 
-  &.router-link-active {
-    color: #2c3e50;
-  }
-}
+//   &.router-link-active {
+//     color: #2c3e50;
+//   }
+// }
 
-.logoutButton {
-  cursor: pointer;
-}
+// .logoutButton {
+//   cursor: pointer;
+// }
 
-.nav-welcome + button {
-  margin-left: 0;
-}
+// .nav-welcome + button {
+//   margin-left: 0;
+// }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <h1>Welcome to the App!</h1>
-    <template v-if="!this.$store.state.user">
+    <!-- header -->
+    <header id="header" class="jumbotron text-center">
+      <div class="container">
+        <h1>Solola</h1>
+        <p>Practice your solo from here.</p>
+        <p>To your stage</p>
+      </div>
+    </header>
+
+    <!-- <template v-if="!this.$store.state.user">
       <div>
         To use the app, you'll need to
         <router-link to="authenticate" @click.native="isNew(false)"
@@ -12,7 +20,7 @@
           >Register</router-link
         >
       </div>
-    </template>
+    </template> -->
   </div>
 </template>
 
@@ -25,3 +33,28 @@ export default {
   },
 };
 </script>
+
+<style>
+/*=============================================
+=                  Header                     =
+=============================================*/
+
+#header {
+  padding: 250px 0;
+  background-image: url('../assets/wallpaper.jpg');
+  background-size: cover;
+  color: #fff;
+}
+
+h1 {
+  font-family: 'Montserrat-Black', sans-serif;
+  font-size: 3.5rem;
+  line-height: 1.5;
+  font-weight: 900;
+}
+
+#header p {
+  font-size: 2.5rem;
+  font-weight: 600;
+}
+</style>
