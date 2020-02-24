aed85b3a8841e6803995b188ea2284c12b0df0fc
YKhorizon <ykhorizon.light@gmail.com>
2020-02-24 20:27:53 +0800

    Revert "SR-131- Add bootstrap (#8)"
    This reverts commit 740db8eade0ee78b7c88884574030c3e652dbeb6.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './vuex/store';
 import axios from 'axios';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
