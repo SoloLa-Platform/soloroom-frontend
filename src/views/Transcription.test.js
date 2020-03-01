e2a21494c603bbfc4a1740381d084ce23b03e0b1
ykhorizon <ykhorizon.light@gmail.com>
2020-03-01 22:36:47 +0800

    SR-138 - Refine login / register (#9)

---
+++ src/views/Transcription.test.js
@@ -0,0 +1,21 @@
+import { createLocalVue, shallowMount, mount } from '@vue/test-utils';
+import Transcription from './Transcription.vue';
+
+const localVue = createLocalVue();
+jest.mock('opensheetmusicdisplay');
+
+describe('test Transcription page', () => {
+    describe('render', () => {
+        it('should render components', () => {
+            const wrapper = shallowMount(Transcription, {
+              stubs: {
+                Editor: {
+                  name: 'Editor',
+                  template: '<div/>',
+                },
+              },
+            });
+            expect(wrapper);
+        });
+    });
+});
--- vue.config.js
+++ vue.config.js
@@ -5,79 +5,145 @@
 const bodyParser = require('body-parser');
 const readline = require('readline');
 const fs = require('fs');
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
-
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
 // const xml = require('xml');
 // const path = require('path');
 
 module.exports = {
-    outputDir: 'dist',
-    assetsDir: 'static',
+  outputDir: 'dist',
+  assetsDir: 'static',
 
-    // chainWebpack: config => {
-    //   config.resolve.alias
-    //       .set('@', path.resolve(__dirname, '/src'));
+  // chainWebpack: config => {
+  //   config.resolve.alias
+  //       .set('@', path.resolve(__dirname, '/src'));
+  // },
+  devServer: {
+    // proxy: {
+    //     '/api*': {
+    //         // Forward frontend dev server request for /api to django dev server
+    //         // target: 'http://localhost:8000/',
+    //     }
     // },
-    devServer: {
-        // proxy: {
-        //     '/api*': {
-        //         // Forward frontend dev server request for /api to django dev server
-        //         // target: 'http://localhost:8000/',
-        //     }
-        // },
-        
-        before(app) {
-          app.use(bodyParser.urlencoded());
-          app.use(bodyParser.json());
-          // Fake user data
-          const FAKE_USER = {
-            email: 'superguitarhero@gmail.com',
-            password: 'Abc12345678',
-            name: 'guitarHero',
-          };
 
-          app.post('/register', (request, response) => {
-            // request
-            const body = request.body;
-            if (body.email === FAKE_USER.email
-              && body.password === FAKE_USER.password) {
-              response.status(409);
-              response.send({result: 'User exist'});
-            } else {
-              response.status(200);
-              response.send({id: `${Math.floor(Math.random()*1000)}`});
-            }
+    before(app) {
+      app.use(cors())
+      app.use(bodyParser.urlencoded());
+      app.use(bodyParser.json());
+
+      // fetch .mzxml
+      app.post('/sheet-music/demo', (request, response) => {
+        let content = '';
+        const reader = readline.createInterface({
+          input: fs.createReadStream(`${__dirname}/mock/MozartTrio.musicxml`),
+        });
+        reader.on('line', (line) => {
+          content += `${line}`;
+        });
+
+        reader.on('close', () => {
+          response.set('Content-Type', 'text/xml');
+          // response.type('application/xml');
+          response.send(content);
+        });
+      });
+
+      // app.get('/', (req, res) => {
+      //   res.json({
+      //     message: 'Welcome to the API.'
+      //   })
+      // })
+
+      app.get('/transcription', verifyToken, (req, res) => {
+        jwt.verify(req.token, 'the_secret_key', err => {
+          if (err) {
+            res.sendStatus(401)
+          }
+        })
+      });
+
+      app.post('/register', (req, res) => {
+        if (req.body) {
+          const user = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+            // In a production app, you'll want to encrypt the password
+          }
+          const data = JSON.stringify(user, null, 2)
+
+          var dbUserEmail = require('./db/user.json').email
+          var errorsToSend = []
+
+          if (dbUserEmail === user.email) {
+            errorsToSend.push('An account with this email already exists.')
+          }
+          if (user.password.length < 5) {
+            errorsToSend.push('Password too short.')
+          }
+          if (errorsToSend.length > 0) {
+            res.status(400).json({
+              errors: errorsToSend
+            })
+          } else {
+            fs.writeFile('./db/user.json', data, err => {
+              if (err) {
+                console.log(err + data)
+              } else {
+                const token = jwt.sign({
+                  user
+                }, 'the_secret_key')
+                // In a production app, you'll want the secret key to be an environment variable
+                res.json({
+                  token,
+                  email: user.email,
+                  name: user.name
+                })
+              }
+            })
+          }
+        } else {
+          res.sendStatus(400)
+        }
+      })
+
+      app.post('/login', (req, res) => {
+        const userDB = fs.readFileSync('./db/user.json')
+        const userInfo = JSON.parse(userDB)
+        if (
+          req.body &&
+          req.body.email === userInfo.email &&
+          req.body.password === userInfo.password
+        ) {
+          const token = jwt.sign({
+            userInfo
+          }, 'the_secret_key');
+          // In a production app, you'll want the secret key to be an environment variable
+          res.json({
+            token,
+            email: userInfo.email,
+            name: userInfo.name
           });
-
-          app.post('/login', (request, response) => {
-            const body = request.body;
-            if (body.email === FAKE_USER.email
-                && body.password === FAKE_USER.password) {
-              response.status(200);
-              response.send({result: 'successfully login'});
-            } else {
-              response.status(401);
-              response.send({result: 'fail to authenticate'});
-            }
+          console.log('success');
+        } else {
+          res.status(401).json({
+            error: 'Invalid login. Please try again.'
           });
-
-
-          // fetch .mzxml
-          app.post('/sheet-music/demo', (request, response) => {
-            let content = '';
-            const reader = readline.createInterface({
-              input: fs.createReadStream(`${__dirname}/mock/MozartTrio.musicxml`),
-            });
-            reader.on('line', (line) => {
-                content += `${line}`;
-            });
-            
-            reader.on('close', () => {
-              response.set('Content-Type', 'text/xml');
-              // response.type('application/xml');
-              response.send(content);
-            });
-          });
-        }        
+          console.log('failed');
+        }
+      })
     }
+  }
 }
