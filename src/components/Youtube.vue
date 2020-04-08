520133992a6509270884bb567061fcb37f308b18
jacktseng0704 <45512754+jacktseng0704@users.noreply.github.com>
2020-04-08 12:41:36 +0800

    SR-150 - Add vue-youtube package (#10)

---
+++ src/components/Youtube.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="youtube">
+    <youtube :video-id="videoId" ref="youtube" width="100%" height="100%" />
+
+    <div class="btn-group">
+      <button @click="playVideo">play</button>
+      <button @click="pauseVideo">pause</button>
+      <button @click="stopVideo">stop</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      videoId: 'lG0Ys-2d4MA',
+    };
+  },
+  methods: {
+    playVideo() {
+      console.log(this);
+      this.player.playVideo();
+    },
+    pauseVideo() {
+      this.player.pauseVideo();
+    },
+    stopVideo() {
+      this.player.stopVideo();
+    },
+    // getVolume() {
+    //   this.value = this.player.getVolume();
+    // },
+    // setVolume() {
+    //   this.player.setVolume(this.value);
+    // },
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn-group {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/index.js';
+import VueYoutube from 'vue-youtube';
+
+Vue.use(VueYoutube);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
