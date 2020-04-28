68fa1a23dd6cd88e6b9d48911fdc5b5d90355536
Jack Tseng <b99901190@gmail.com>
2020-04-28 23:30:13 +0800

    SR-150 - Add Font-Awesome

--- src/components/PlayingController.vue
+++ src/components/PlayingController.vue
@@ -1,48 +1,90 @@
 <template>
   <div class="playing-controller">
-    <button @click="playVideo">play</button>
-    <button @click="pauseVideo">pause</button>
-    <button @click="stopVideo">stop</button>
+    <vue-slider v-model="value"></vue-slider>
+    <div @click="playVideo">
+      <font-awesome-icon class="controlIcons" size="1.5x" icon="play" />
+    </div>
+    <div @click="pauseVideo">
+      <font-awesome-icon class="controlIcons" size="1.5x" icon="pause" />
+    </div>
 
-    <div class="volume">
-      <label for="volume">volume</label>
-      <input
-        @click="getVolume"
-        @input="setVolume"
-        type="range"
-        min="0"
-        max="100"
-        step="1"
+    <div @click="setRepeatStatus()">
+      <font-awesome-icon
+        class="controlIcons"
+        icon="redo"
+        title="Repeat"
+        size="1.5x"
       />
     </div>
+
+    <font-awesome-icon
+      @mouseenter="volumeVisible = true"
+      class="controlIcons"
+      :icon="volume === 0 ? 'volume-mute' : 'volume-up'"
+      size="1.5x"
+    />
+
+    <transition name="fade">
+      <div
+        class="volume-slider"
+        @mouseleave="volumeVisible = false"
+        v-if="volumeVisible"
+      >
+        <vue-slider
+          direction="btt"
+          v-model="volume"
+          :min="0"
+          :max="1"
+          :height="100"
+          :interval="0.02"
+          :tooltip-placement="'right'"
+          :tooltip-formatter="value => Math.round(value * 100)"
+        />
+      </div>
+    </transition>
+
+    <button @click="stopVideo">stop</button>
   </div>
 </template>
 
 <script>
+import VueSlider from 'vue-slider-component';
+import 'vue-slider-component/theme/default.css';
+
 export default {
+  name: 'PlayerController',
+  components: { VueSlider },
   data() {
-    return {};
+    return {
+      volumeVisible: false,
+      isPlaying: false,
+    };
+  },
+  computed: {
+    volume: {
+      get() {
+        return this.volumeState;
+      },
+      set(value) {
+        this.setVolume({ volume: value });
+      },
+    },
   },
   methods: {
     playVideo() {
-      this.player.playVideo();
+      this.$bus.$emit('playVideo');
     },
     pauseVideo() {
-      this.player.pauseVideo();
+      this.$bus.$emit('pauseVideo');
     },
     stopVideo() {
-      this.player.stopVideo();
+      this.$bus.$emit('stopVideo');
     },
     getVolume() {
-      this.value = this.player.getVolume();
+      this.$bus.$emit('getVolume');
     },
     setVolume() {
-      this.player.setVolume(this.value);
-    },
-  },
-  computed: {
-    player() {
-      return this.$refs.youtube.player;
+      this.$bus.$emit('setVolume');
     },
   },
 };
@@ -50,7 +92,43 @@ export default {
 
 <style>
 .playing-controller {
+  box-sizing: border-box;
   display: flex;
+  align-self: center;
+  flex-shrink: 0;
+  width: 100%;
+  height: 100px;
   justify-content: space-evenly;
+  align-items: center;
+  background-color: #bbb;
+}
+
+.controlIcons {
+  padding: 15px;
+  cursor: pointer;
+  border-radius: 50%;
+  background-color: #ddd;
+  color: #333;
+}
+
+.volume-slider {
+  display: flex;
+  padding: 10px;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 130px;
+  background: rgba(255, 255, 255, 0.6);
+  position: absolute;
+  bottom: 0;
+  left: -10px;
+  z-index: 10;
+  border-radius: 10px;
+  color: black;
+}
+
+.scrubber {
+  color: black;
+  width: 100px;
 }
 </style>
--- src/components/Youtube.vue
+++ src/components/Youtube.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="youtube">
     <youtube :video-id="videoId" ref="youtube" width="100%" height="100%" />
-
-    <div class="btn-group">
-      <button @click="playVideo">play</button>
-      <button @click="pauseVideo">pause</button>
-      <button @click="stopVideo">stop</button>
-    </div>
   </div>
 </template>
 
@@ -15,11 +9,28 @@ export default {
   data() {
     return {
       videoId: 'lG0Ys-2d4MA',
+      volume: 0.8,
     };
+  },
+  mounted() {
+    this.bus.$on('playVideo', () => {
+      this.playVideo();
+    }),
+      this.bus.$on('pauseVideo', () => {
+        this.pauseVideo();
+      }),
+      this.bus.$on('stopVideo', () => {
+        this.stopVideo();
+      }),
+      this.bus.$on('getVolume', () => {
+        return this.getVolume();
+      }),
+      this.bus.$on('setVolume', () => {
+        this.setVolume();
+      });
   },
   methods: {
     playVideo() {
-      console.log(this);
       this.player.playVideo();
     },
     pauseVideo() {
@@ -28,12 +39,12 @@ export default {
     stopVideo() {
       this.player.stopVideo();
     },
-    // getVolume() {
-    //   this.value = this.player.getVolume();
-    // },
-    // setVolume() {
-    //   this.player.setVolume(this.value);
-    // },
+    getVolume() {
+      this.value = this.player.getVolume();
+    },
+    setVolume(val) {
+      this.player.setVolume(this.value);
+    },
   },
   computed: {
     player() {
@@ -43,9 +54,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.btn-group {
-  display: flex;
-  justify-content: space-evenly;
-}
-</style>
+<style scoped></style>
